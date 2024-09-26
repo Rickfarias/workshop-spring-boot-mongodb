88ccb38201d8ee8b5ed1844c6b09a42dccc57d7a
Rick Farias <167890284+Rickfarias@users.noreply.github.com>
2024-09-26 10:23:08 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/rickfarias/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rickfarias.workshopmongo.repository;
+
+import com.rickfarias.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}

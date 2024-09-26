9bf43d158f2d7c6993d86fe27c72f1f48628be44
Rick Farias <167890284+Rickfarias@users.noreply.github.com>
2024-09-26 09:10:20 -0300

    primeiro commit

---
+++ src/main/java/com/rickfarias/workshopmongo/Workshopmongo1Application.java
@@ -0,0 +1,12 @@
+package com.rickfarias.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopmongo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopmongo1Application.class, args);
+	}
+}
